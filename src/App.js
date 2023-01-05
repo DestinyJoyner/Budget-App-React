f90b5db13b96c655291dc9850b19d93c1ebea08b
DestinyJoyner <destinyjoyner@pursuit.org>
2023-01-04 22:32:54 -0500

    created provider component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
-import {useEffect, useState} from "react"
-import axios from 'axios';
+import Provider from "./Components/Provider";
 import RouteComponent from "./Components/RouteComponent";
 import './App.css';
 
 function App() {
-  const API = process.env.REACT_APP_API_URL
-  const [data, setData] = useState([])
-  useEffect(() => {
-    axios.get(`${API}`)
-    .then(respJson => setData(respJson.data) )
-    .catch(err => console.log(err))
-  },[])
   return (
     <div className="App">
+      <Provider>
       <RouteComponent />
-      
+      </Provider>
     </div>
   );
 }
---
+++ src/Components/Provider.js
@@ -0,0 +1,33 @@
+import { createContext, useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import Nav from './Nav'
+import Footer from './Footer'
+export const ContextData = createContext()
+
+function Provider({children}) {
+    const API = process.env.REACT_APP_API_URL
+    const [data, setData] = useState([])
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        axios.get(`${API}`)
+        .then(respJson => setData(respJson.data) )
+        .catch(err => navigate("/*"))
+      },[])
+
+    return (
+       <ContextData.Provider value = {{
+        axios,
+        API,
+        data,
+        setData,
+       }}>
+        <Nav />
+        <Footer />
+        {children}
+       </ContextData.Provider>
+    );
+}
+
+export default Provider;
