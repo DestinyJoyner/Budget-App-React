ba21ee7258259b0f036103943c284fcab0e735a5
DestinyJoyner <destinyjoyner@pursuit.org>
2023-01-04 20:06:37 -0500

    can fetch data from deployed backend website

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import {useEffect, useState} from "react"
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const API = process.env.REACT_APP_API_URL
+  const [data, setData] = useState([])
+  useEffect(() => {
+    axios.get(`${API}`)
+    .then(respJson => setData(respJson.data) )
+    .catch(err => console.log(err))
+  },[])
   return (
     <div className="App">
       <h1>Pursuit App Template</h1>
+      {data.length > 0 && Object.keys(data[0])}
+      
     </div>
   );
 }
