8153c6d0a6a4a3403f3c537051b07cd78a81991b
DestinyJoyner <destinyjoyner@pursuit.org>
2023-01-06 17:15:37 -0500

    basic display of elements on show page

--- src/Components/BudgetShow.js
+++ src/Components/BudgetShow.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import { useState, useEffect, useContext } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { ContextData } from "./Provider";
+import { convertDate } from "../ReusableComponents/helperFunctions";
+import "./BudgetShow.css"
 
-function BudgetShow(props) {
+function BudgetShow() {
+    const{API, axios} = useContext(ContextData)
+    const {id} = useParams()
+    const navigate = useNavigate()
+    const [thisTransaction, setThisTransaction] = useState({})
+
+
+    useEffect(() => {
+        axios.get(`${API}/${id}`)
+        .then(respJson => setThisTransaction(respJson.data))
+        .catch(err => navigate("/*"))
+    })
+
     return (
-        <div>
-            Show
+        <div className='show'>
+            <h1>Transaction</h1>
+            {
+                Object.keys(thisTransaction).length &&
+                <>
+                    <p>{convertDate(thisTransaction.date)}</p>
+                    <p>{thisTransaction.itemName}</p>
+                    <p>{Math.abs(thisTransaction.amount)}</p>
+                    <p>{thisTransaction.from}</p>
+                    <p>{thisTransaction.category}</p>
+                </>
+            }
         </div>
     );
 }
