15e49ff75aed536a86717eb2835c95bfef039e1f
DestinyJoyner <destinyjoyner@pursuit.org>
2023-01-07 01:06:55 -0500

    added banknote font, nav css, index page background, index css good

--- src/Components/BudgetIndexDisplay.js
+++ src/Components/BudgetIndexDisplay.js
@@ -6,8 +6,11 @@ function BudgetIndexDisplay({date, itemName, amount, id}) {
     return ( 
         <div className="transaction">
             <img src={payment} alt="wallet-icon" />
+
             <p>{convertDate(date)}</p>
+
             <Link to={`/transactions/${id}`}><p>{itemName}</p></Link>
+            
             <p
             style={{color: amount < 0 ? "red" : "green"}}>{amount < 0 ? "- " : ''}${Math.abs(amount).toFixed(2)}</p>
         </div>
