c6eaa7eb516739fbfccef6e799faa707b02a1975
DestinyJoyner <destinyjoyner@pursuit.org>
2023-01-06 20:26:16 -0500

    show page css complete

--- src/Components/BudgetShow.js
+++ src/Components/BudgetShow.js
@@ -2,16 +2,17 @@ import { useState, useEffect, useContext } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { ContextData } from "./Provider";
 import { convertDate } from "../ReusableComponents/helperFunctions";
+import { toWords } from "number-to-words";
 import income from "../assets/income.png"
 import expense from "../assets/expense.png"
 import "./BudgetShow.css"
+
 
 function BudgetShow() {
     const{API, axios} = useContext(ContextData)
     const {id} = useParams()
     const navigate = useNavigate()
     const [thisTransaction, setThisTransaction] = useState({})
-
 
     useEffect(() => {
         axios.get(`${API}/${id}`)
@@ -26,18 +27,48 @@ function BudgetShow() {
                 Object.keys(thisTransaction).length &&
                 <div className="transacDetails">
                     <h3>Receipt</h3>
-                    <img src={thisTransaction.amount > 0 ? income : expense} alt = "income-expense"/>
-                    <div className="date">
-                        <p>Date: <span>{convertDate(thisTransaction.date)}</span></p>
-                    </div>
+
+                    {/* <img 
+                    src={thisTransaction.amount > 0 ? income : expense} 
+                    alt = "income-expense"/> */}
+
+                    <p className="date">Date: 
+                        <span>
+                            {convertDate(thisTransaction.date)}
+                        </span>
+                    </p>
                     
-                    <div className="name-amount">
-                    <p>For:<span>{thisTransaction.itemName}</span></p>
-                    <p>Amount: $<span>{Math.abs(thisTransaction.amount)}</span></p>
-                    <p>From: <span>{thisTransaction.from}</span></p>
-                    <p>Memo: <span>{thisTransaction.category}</span></p>
+                    <p className="for">For:
+                        <span>{thisTransaction.itemName}
+                        </span>
+                    </p>
+                    
+                    <p className="amount">$
+                        <span>
+                            {Math.abs(thisTransaction.amount)}</span>
+                    </p>
+                   
+                    <div className="transac-data">
+                    <p>In the Amount of:
+                        <span>
+                            {toWords(Math.abs(thisTransaction.amount))}
+                        </span> Dollars
+                    </p>
+                    <p>From: 
+                        <span>
+                            {thisTransaction.from}
+                            </span>
+                    </p>
+                    <p>Memo: 
+                        <span>
+                            {thisTransaction.category}
+                            </span>
+                    </p>
                     </div>
-                    <p className="receipt-id">{thisTransaction.id}</p>
+
+                    <p className="receipt-id">
+                        {thisTransaction.id}
+                    </p>
                     
                 </div>
             }
