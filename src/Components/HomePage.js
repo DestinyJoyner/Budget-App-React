9c13605c3cbddac83e7dd6726972b1553cc23bef
DestinyJoyner <destinyjoyner@pursuit.org>
2023-01-10 16:46:07 -0500

    homePage media complete, added transaction button as well, updated css

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,12 +1,16 @@
 import { useContext , useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { ContextData } from "./Provider";
 import HomeModal from "./HomeModal";
+import altHomeImg from "../assets/cash-fan.png"
 import homeImg from "../assets/budget-home.png"
 import dollarSign from "../assets/spin-dollar(2).gif"
+import receipt from "../assets/receipt-gif.gif"
 import "./HomePage.css"
 
 function HomePage() {
     const {setHomeModal, originalTotal} = useContext(ContextData)
+    const navigate = useNavigate()
 
     useEffect(() => {
         if(!originalTotal) setHomeModal(true)
@@ -21,8 +25,14 @@ function HomePage() {
             onClick={() => setHomeModal(true)}>
                 <img src={dollarSign} alt="dollar" />
             </button>
+            <button 
+            className="circle1 homeTransac"
+            onClick={() => navigate("/transactions")}>
+                <img src={receipt} alt="transac-icon" />
+            </button>
             <section className="homePage">
-                <img src={homeImg} alt="home" />
+                <img src={homeImg} alt="home" className="origHome" />
+                <img src ={altHomeImg} alt="home" className="mediaHome" />
             </section>
             <p>Destiny J.</p>
         </div>
